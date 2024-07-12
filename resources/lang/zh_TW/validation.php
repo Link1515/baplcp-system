83fabe031a8a5fcf7b432fbd535ccbfda22cc6ca
Link1515 <h26281660@gmail.com>
2024-07-12 15:06:42 +0800

    fix validation i18n

--- resources/lang/zh_TW/validation.php
+++ resources/lang/zh_TW/validation.php
@@ -2,17 +2,28 @@
 
 return [
     'required' => '請輸入:attribute',
-    'required_with' => '當:values時，請輸入:attribute',
-    'eventDates' => ':attribute必須要是合法的日期格式 "Y-m-d, Y-m-d, ..."',
+    'required_if' => '當:other時，請輸入:attribute',
+    'max' => [
+        'numeric' => ':attribute不可超過 :max',
+        'string' => ':attribute不得大於 :max 個字元',
+        'array' => ':attribute不得多於 :max 個項目'
+    ],
+    'lt' => [
+        'numeric' => ':attribute必須小於 :value',
+    ],
+    'eventDates' => ':attribute必須要是合法的格式: "Y-m-d, Y-m-d, ..."',
     'attributes' => [
         'title' => '標題',
         'subTitle' => '副標題',
         'eventPrice' => '單次費用',
-        'eventMaxParticipants' => '最大參與人數',
+        'eventMemberParticipants' => '群內人數',
+        'eventNonMemberParticipants' => '群外人數',
         'eventTime' => '活動開始時間',
         'eventDates' => '日期',
         'eventStartRegisterDayBefore' => '開放報名時間',
+        'eventStartRegisterDayBeforeTime' => '開放報名時間',
         'eventEndRegisterDayBefore' => '截止報名時間',
+        'eventEndRegisterDayBeforeTime' => '截止報名時間',
         'canRegisterAllEvent' => '開放報名季打',
         'eventGroupPrice' => '季打費用',
         'eventGroupMaxParticipants' => '季打名額',

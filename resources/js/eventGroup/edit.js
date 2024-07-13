1ca5a677b97ec550b59e7cf69c7820b0e4c4b80c
Link1515 <h26281660@gmail.com>
2024-07-13 12:23:38 +0800

    refactoring eventGroup edit page

--- resources/js/eventGroup/edit.js
+++ resources/js/eventGroup/edit.js
@@ -2,7 +2,6 @@ import flatpickr from "flatpickr";
 import { MandarinTraditional } from "flatpickr/dist/l10n/zh-tw";
 import "flatpickr/dist/flatpickr.min.css";
 import { format, subDays } from "date-fns";
-import axios from "axios";
 
 const registerEndDateForEventGroupFlatpickr = flatpickr(
     "#eventGroupRegisterEndAt",
@@ -46,9 +45,8 @@ const registerStartDateForEventGroupFlatpickr = flatpickr(
 
 window.deleteGroupEvent = function (url, redirct) {
     window.popup.confirm("是否確定刪除季打？", (result) => {
-        if (result.isDenied) return;
-        window.axios.delete(url);
-        axios.delete(url).then(({ status }) => {
+        if (result.isDenied || result.isDismissed) return;
+        window.axios.delete(url).then(({ status }) => {
             if (status === 200) {
                 window.popup.success("刪除成功", () => {
                     window.location.href = redirct;
--- resources/views/admin/eventGroup/edit.blade.php
+++ resources/views/admin/eventGroup/edit.blade.php
@@ -2,6 +2,36 @@
 
 @php
     use Carbon\Carbon;
+
+    function defaultValue($table, $formField, $tableColumn = null)
+    {
+        if (is_null($tableColumn)) {
+            $tableColumn = $formField;
+        }
+        return old('formSubmitted') ? old($formField) : $table[$tableColumn];
+    }
+
+    $canRegisterAllEventDefaultValule = old('formSubmitted')
+        ? (old('canRegisterAllEvent')
+            ? 'checked'
+            : '')
+        : ($eventGroup['can_register_all_event']
+            ? 'checked'
+            : '');
+
+    $eventGroupRegisterStartAtDefaultValue = $eventGroup['register_start_at']
+        ? Carbon::parse($eventGroup['register_start_at'])->format('Y-m-d H:i')
+        : old('eventGroupRegisterStartAt');
+
+    $eventGroupRegisterEndAtDefaultValue = $eventGroup['register_end_at']
+        ? Carbon::parse($eventGroup['register_end_at'])->format('Y-m-d H:i')
+        : old('eventGroupRegisterEndAt');
+
+    $deleteGroupEventMethod = sprintf(
+        "deleteGroupEvent('%s', '%s')",
+        route('admin.eventGroups.destroy', ['eventGroup' => $eventGroup->id]),
+        route('admin.eventGroups.index'),
+    );
 @endphp
 
 @section('js')
@@ -21,7 +51,7 @@
             singlePrice: '',
             memberParticipants: '',
             nonMemberParticipants: '',
-    
+
             canRegisterAllEvent: '',
             registerAllPrice: '',
             registerAllParticipants: '',
@@ -31,102 +61,61 @@
     }" class="grid gap-4">
         @csrf
         <input type="hidden" name="formSubmitted" value="true">
-        <label class="flex flex-col gap-1 text-lg">
+        <x-forms.input field="title" :defaultValue="defaultValue(table: $eventGroup, formField: 'title')" type="text">
             標題
-            <input id="title" x-model.fill="form.title" type="text" name="title"
-                value="{{ old('title') ?? $eventGroup['title'] }}" class="input @error('title') is-invalid @enderror">
-            @error('title')
-                <div class="text-red-600">{{ $message }}</div>
-            @enderror
-        </label>
-        <label class="flex flex-col gap-1 text-lg">
+        </x-forms.input>
+        <x-forms.input field="subTitle" :defaultValue="defaultValue(table: $eventGroup, formField: 'subTitle', tableColumn: 'sub_title')" type="text">
             副標題
-            <input id="subTitle" x-model.fill="form.subTitle" type="text" name="subTitle"
-                value="{{ old('subTitle') ?? $eventGroup['sub_title'] }}"
-                class="input @error('subTitle') is-invalid @enderror">
-            @error('subTitle')
-                <div class="text-red-600">{{ $message }}</div>
-            @enderror
-        </label>
-        <label class="flex flex-col gap-1 text-lg">
+        </x-forms.input>
+        <x-forms.input field="singlePrice" :defaultValue="defaultValue(table: $eventGroup, formField: 'singlePrice', tableColumn: 'price')" type="number">
             單次費用
-            <input id="singlePrice" x-model.fill="form.singlePrice" type="number" name="singlePrice"
-                value="{{ old('singlePrice') ?? $eventGroup['price'] }}"
-                class="input @error('singlePrice') is-invalid @enderror">
-            @error('singlePrice')
-                <div class="text-red-600">{{ $message }}</div>
-            @enderror
-        </label>
-        <label class="flex flex-col gap-1 text-lg">
+        </x-forms.input>
+        <x-forms.input field="memberParticipants" :defaultValue="defaultValue(
+            table: $eventGroup,
+            formField: 'memberParticipants',
+            tableColumn: 'member_participants',
+        )" type="number">
             群內人數
-            <input id="memberParticipants" x-model.fill="form.memberParticipants" type="number" name="memberParticipants"
-                value="{{ old('memberParticipants') ?? $eventGroup['member_participants'] }}"
-                class="input @error('memberParticipants') is-invalid @enderror">
-            @error('memberParticipants')
-                <div class="text-red-600">{{ $message }}</div>
-            @enderror
-        </label>
-        <label class="flex flex-col gap-1 text-lg">
+        </x-forms.input>
+        <x-forms.input field="nonMemberParticipants" :defaultValue="defaultValue(
+            table: $eventGroup,
+            formField: 'nonMemberParticipants',
+            tableColumn: 'non_member_participants',
+        )" type="number">
             群外人數
-            <input id="nonMemberParticipants" x-model.fill="form.nonMemberParticipants" type="number"
-                name="nonMemberParticipants"
-                value="{{ old('nonMemberParticipants') || $eventGroup['non_member_participants'] }}"
-                class="input @error('nonMemberParticipants') is-invalid @enderror">
-            @error('nonMemberParticipants')
-                <div class="text-red-600">{{ $message }}</div>
-            @enderror
-        </label>
+        </x-forms.input>
+
         <label class="flex items-center gap-2 text-lg">
             <input x-model.fill="form.canRegisterAllEvent" id="canRegisterAllEvent" value="true"
-                {{ old('formSubmitted') ? (old('canRegisterAllEvent') ? 'checked' : '') : ($eventGroup['can_register_all_event'] ? 'checked' : '') }}
-                type="checkbox" name="canRegisterAllEvent" />
+                {{ $canRegisterAllEventDefaultValule }} type="checkbox" name="canRegisterAllEvent" />
             開放報名季打
         </label>
+
         <div x-show="form.canRegisterAllEvent" class="flex flex-col gap-4" x-collapse>
-            <label class="flex flex-col gap-1 text-lg">
+            <x-forms.input field="registerAllPrice" :defaultValue="defaultValue(
+                table: $eventGroup,
+                formField: 'registerAllPrice',
+                tableColumn: 'register_all_price',
+            )" type="number">
                 季打費用
-                <input id="registerAllPrice" x-model.fill="form.registerAllPrice" type="number" name="registerAllPrice"
-                    value="{{ $eventGroup['register_all_price'] }}"
-                    class="input @error('registerAllPrice') is-invalid @enderror">
-                @error('registerAllPrice')
-                    <div class="text-red-600">{{ $message }}</div>
-                @enderror
-            </label>
-            <label class="flex flex-col gap-1 text-lg">
+            </x-forms.input>
+            <x-forms.input field="registerAllParticipants" :defaultValue="defaultValue(
+                table: $eventGroup,
+                formField: 'registerAllParticipants',
+                tableColumn: 'register_all_participants',
+            )" type="number">
                 季打名額
-                <input id="registerAllParticipants" x-model.fill="form.registerAllParticipants" type="number"
-                    name="registerAllParticipants" value="{{ $eventGroup['register_all_participants'] }}"
-                    class="input @error('registerAllParticipants') is-invalid @enderror">
-                @error('registerAllParticipants')
-                    <div class="text-red-600">{{ $message }}</div>
-                @enderror
-            </label>
-            <label class="flex flex-col gap-1 text-lg">
+            </x-forms.input>
+            <x-forms.input field="eventGroupRegisterStartAt" :defaultValue="$eventGroupRegisterStartAtDefaultValue" type="text">
                 季打開放報名時間
-                <input id="eventGroupRegisterStartAt" x-model.fill="form.eventGroupRegisterStartAt" type="text"
-                    name="eventGroupRegisterStartAt"
-                    value="{{ $eventGroup['register_start_at'] ? Carbon::parse($eventGroup['register_start_at'])->format('Y-m-d H:i') : '' }}"
-                    class="input @error('eventGroupRegisterStartAt') is-invalid @enderror">
-                @error('eventGroupRegisterStartAt')
-                    <div class="text-red-600">{{ $message }}</div>
-                @enderror
-            </label>
-            <label class="flex flex-col gap-1 text-lg">
+            </x-forms.input>
+            <x-forms.input field="eventGroupRegisterEndAt" :defaultValue="$eventGroupRegisterEndAtDefaultValue" type="text">
                 季打結束報名時間
-                <input id="eventGroupRegisterEndAt" x-model.fill="form.eventGroupRegisterEndAt" type="text"
-                    name="eventGroupRegisterEndAt"
-                    value="{{ $eventGroup['register_end_at'] ? Carbon::parse($eventGroup['register_end_at'])->format('Y-m-d H:i') : '' }}"
-                    class="input @error('eventGroupRegisterEndAt') is-invalid @enderror">
-                @error('eventGroupRegisterEndAt')
-                    <div class="text-red-600">{{ $message }}</div>
-                @enderror
-            </label>
+            </x-forms.input>
         </div>
 
         <div class="grid grid-cols-2 gap-4 mt-6">
-            <button
-                @click="deleteGroupEvent('{{ route('admin.eventGroups.destroy', ['eventGroup' => $eventGroup->id]) }}', '{{ route('admin.eventGroups.index') }}')"
-                class="btn-submit bg-red-500" type="button">
+            <button @click="{{ $deleteGroupEventMethod }}" class="btn-submit bg-red-500" type="button">
                 刪除季打
             </button>
             <button class="btn-submit" :disabled="form.processing">
